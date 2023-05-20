8a2ca3407c013228e26cef121b24000484ad06cd
Debian <admin@ip-172-31-3-234.ec2.internal>
2023-05-20 17:08:46 +0000

    authentication update : support both http and https and retrieve email from logged user

--- auth/index.js
+++ auth/index.js
@@ -1,12 +1,25 @@
-// server/index.js
+
 const express = require('express');
 const axios = require('axios');
 const qs = require('qs');
+const session = require('express-session');
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 
 const app = express();
 const port = 3000; // Update with your desired port number
+
+// Load environment variables
+dotenvExpand.expand(dotenv.config());
+
+// Configure session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // Replace with your session secret
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.get('/', (req, res) => {
   // Retrieve the authorization code from the query parameters
@@ -26,14 +39,26 @@ app.get('/', (req, res) => {
     .then(response => {
       // Handle the token exchange response
       const tokens = response.data;
-      // Do further processing or store the tokens as per your requirements
-      console.log(tokens);
+      console.log(tokens); // Log the tokens object to inspect its structure
+
+      // Store the email in the session for later use
+      req.session.email = tokens.email || tokens.email_verified; // Use the appropriate property name
+
       res.send('Token exchange successful!'); // Send a response indicating success
     })
     .catch(error => {
       console.error('Token exchange failed:', error);
       res.status(500).send('Token exchange failed'); // Send an error response
     });
+});
+
+app.get('/uploads', (req, res) => {
+  // Access the email stored in the session
+  const email = req.session.email;
+
+  // Use the email for further processing or retrieve data for that user
+
+  res.send(`Email: ${email}`);
 });
 
 app.listen(port, () => {
